11b749a67bc2df53c936fc7aaed56acf9b536938
Maxwell134 <maxbasumatry@gmail.com>
2024-12-27 03:16:12 +0530

    added changed

---
+++ issue.py
@@ -0,0 +1,38 @@
+import subprocess
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from termcolor import colored
+load_dotenv()
+repo_owner = 'maxwell134'
+repo_name = 'CI-CD'
+GIT_TOKEN = os.getenv("GIT_TOKEN")
+
+def create_issue(repo_owner, repo_name):
+    url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/issues'
+    tasks = ["rollback", "scale_down", "scale_up"]
+    body = '\n'.join([f'- [ ] {task}' for task in tasks])
+
+    issue = {
+        "title": 'Perform following operations task',
+        "body": body,
+        "assignees": ["maxwell134"],
+        "labels": tasks
+    }
+
+    response = requests.post(url, headers={"Authorization": f"token {GIT_TOKEN}"},
+                             data=json.dumps(issue))
+
+    if response.status_code == 201:
+        issue_url = response.json()['html_url']
+        # Print the URL in green color
+        print(f"Successfully created issue. View it at: {colored(issue_url, 'green')}")
+    else:
+        print("Failed to create issue.")
+        print(response.text)
+
+
+# Example usage
+create_issue(repo_owner, repo_name)
+
